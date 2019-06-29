04302caa42032e7d5ea22dcf8a50063ad0a517a4
lutong <983568447@qq.com>
2019-06-29 16:29:39 +0800

    update

--- jx_managent/src/main/java/com/jingxiang/common/config/WebSecurityConfiguration.java
+++ jx_managent/src/main/java/com/jingxiang/common/config/WebSecurityConfiguration.java
@@ -7,11 +7,13 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
 	@Autowired
     private UserDetailsService userDetailsService;
