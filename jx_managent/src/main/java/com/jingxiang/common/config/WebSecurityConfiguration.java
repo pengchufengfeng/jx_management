670ab1483d4d757b341a297df85eb2f887db2dfe
lutong <983568447@qq.com>
2019-07-01 18:25:00 +0800

    merge

--- jx_managent/src/main/java/com/jingxiang/common/config/WebSecurityConfiguration.java
+++ jx_managent/src/main/java/com/jingxiang/common/config/WebSecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,11 +14,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
-<<<<<<< HEAD
 @EnableWebSecurity
-=======
 @EnableGlobalMethodSecurity(prePostEnabled = true, jsr250Enabled = true)
->>>>>>> 97d05460d392f197ac0b8cb65eb7fd22c2468a7d
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
 	@Autowired
     private UserDetailsService userDetailsService;
